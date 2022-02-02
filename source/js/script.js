7cd1428d77f032b2a84660fadab2f8ea432b27da
Petro Melnyk <watcherclub5@gmail.com>
2022-02-02 12:03:56 +0200

    Responsive web page about bikes!

---
+++ source/js/script.js
@@ -0,0 +1,34 @@
+$('.wrapper').addClass('loaded');
+
+$('.icon-menu').click(function(event) {
+  $(this).toggleClass('active');
+  $('.menu__body').toggleClass('active');
+  $('body').toggleClass('lock');
+});
+
+function ibg(){
+  let ibg = document.querySelectorAll(".ibg");
+    for (let i = 0; i < ibg.length; i++) {
+      if(ibg[i].querySelector('img')){
+        ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector  ('img').getAttribute('src')+')';
+    }
+  }
+}
+ibg();
+
+if($('.slider__body').length>0){
+  $('.slider__body').slick({
+    dots:true,
+    arrows: false,
+    accessibility: false,
+    slidesToShow: 1,
+    autoplaySpeed: 3000,
+    adaptiveHeight: true,
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    preArrow: '<button type="button" class="slick-prev"></button>',
+    responsive: [{
+      breakpoint: 768,
+      settings: {}
+    }]
+  });
+}
